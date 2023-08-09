602f82271d0105d46c2ed84ef03db8d84206ddb5
Kevin Brabants <killerkid36912@gmail.com>
2023-08-08 22:13:46 -0400

    update

--- SecuritySite/Pages/cart.cshtml.cs
+++ SecuritySite/Pages/cart.cshtml.cs
@@ -1,27 +1,80 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SecuritySite.Models;
+using System.Reflection;
 
 namespace SecuritySite.Pages
 {
     public class cartModel : PageModel
     {
         public Dictionary<string, int> priceDirectory { get; } = new Dictionary<string, int>();
+        public Dictionary<string, string> planDirectory { get; } = new Dictionary<string, string>();
+        public int Cost { get; set; }
+        public cartModel()
+        {
+            priceDirectory["diy-r-s"] = 20;
+            priceDirectory["diy-r-g"] = 25;
+            priceDirectory["diy-r-cam-s"] = 32;
+
+            priceDirectory["pro-r-s"] = 25;
+            priceDirectory["pro-r-g"] = 30;
+
+            priceDirectory["pro-r-cam-s"] = 40;
+            priceDirectory["pro-r-cam-g"] = 45;
+
+            priceDirectory["diy-c-s"] = 30;
+            priceDirectory["diy-c-g"] = 35;
+            priceDirectory["diy-c-cam-s"] = 45;
+
+            priceDirectory["pro-c-s"] = 45;
+            priceDirectory["pro-c-g"] = 55;
+
+            priceDirectory["pro-c-cam-s"] = 65;
+            priceDirectory["pro-c-cam-g"] = 70;
+
+            //plan directory
+
+            planDirectory["diy-r-s"] = "Residential DIY Silver Monitoring";
+            planDirectory["diy-r-g"] = "Residential DIY Gold Monitoring";
+            planDirectory["diy-r-cam-s"] = "Residential DIY Monitoring With Cameras";
+
+            planDirectory["pro-r-s"] = "Residential Silver Monitoring";
+            planDirectory["pro-r-g"] = "Residential Gold Monitoring";
+
+            planDirectory["pro-r-cam-s"] = "Residential Monitoring With Cameras (Silver)";
+            planDirectory["pro-r-cam-g"] = "Residentail Monitoring With Cameras (Gold)";
+
+            planDirectory["diy-c-s"] = "Commercial DIY Silver Monitoring";
+            planDirectory["diy-c-g"] = "Commercial DIY Gold Monitoring";
+            planDirectory["diy-c-cam-s"] = "Commercial Monitoring With Cameras";
+
+            planDirectory["pro-c-s"] = "Commercial Monitoring (Silver)";
+            planDirectory["pro-c-g"] = "Commercial Monitoring (Gold)"; ;
+
+            planDirectory["pro-c-cam-s"] = "Commercial Monitoring With Cameras (Silver)";
+            planDirectory["pro-c-cam-g"] = "Commercial Monitoring With Cameras (Gold)";
+
+            MonitoringInfo = new();
+        }
 
 
         [BindProperty(SupportsGet = true)]
-        public string monitoringCode { get; set; }
+        public string MonitoringCode { get; set; }
 
         public Input MonitoringInfo { get; set; }
         public ActionResult OnGet()
         {
-            if (priceDirectory[monitoringCode] != null)
+            if(MonitoringCode == null)
+                return RedirectToPage("index");
+
+            if (priceDirectory.TryGetValue(MonitoringCode,out int cost))
             {
-                MonitoringInfo.cost = priceDirectory[monitoringCode];
+                Cost = cost;
                 return Page();
             }
 
-           return RedirectToPage("/");
+           return RedirectToPage("index");
         }
 
 
@@ -29,11 +82,7 @@ namespace SecuritySite.Pages
 
     public class Input
     {
-        public string name { get; set; }
-        public string address { get; set; }
-        public string city { get; set; }
-        public string state { get; set; }
-        public string zipcode { get; set; }
-        public int cost { get; set; }
+        public CustomerInformation customer { get; set; }
+        
     }
 }
